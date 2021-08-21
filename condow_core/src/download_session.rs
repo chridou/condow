eec956d07dd9e2e357cf077f2222864dba6443fb
Christian Douven <chridou@users.noreply.github.com>
2021-08-21 14:40:09 +0200

    implemented composite report

--- condow_core/src/download_session.rs
+++ condow_core/src/download_session.rs
@@ -203,19 +203,40 @@ where
 struct CompositeReporter<RA: Reporter, RB: Reporter>(RA, RB);
 
 impl<RA: Reporter, RB: Reporter> Reporter for CompositeReporter<RA, RB> {
-    fn effective_range(&self, range: crate::InclusiveRange) {}
+    fn effective_range(&self, range: crate::InclusiveRange) {
+        self.0.effective_range(range);
+        self.1.effective_range(range);
+    }
 
-    fn download_started(&self) {}
+    fn download_started(&self) {
+        self.0.download_started();
+        self.1.download_started();
+    }
 
-    fn download_completed(&self) {}
+    fn download_completed(&self) {
+        self.0.download_completed();
+        self.1.download_completed();
+    }
 
-    fn download_failed(&self) {}
+    fn download_failed(&self) {
+        self.0.download_failed();
+        self.1.download_failed();
+    }
 
-    fn queue_full(&self) {}
+    fn queue_full(&self) {
+        self.0.queue_full();
+        self.1.queue_full();
+    }
 
-    fn chunk_completed(&self, part_index: usize, n_bytes: usize, time: std::time::Duration) {}
+    fn chunk_completed(&self, part_index: usize, n_bytes: usize, time: std::time::Duration) {
+        self.0.chunk_completed(part_index, n_bytes, time);
+        self.1.chunk_completed(part_index, n_bytes, time);
+    }
 
-    fn part_started(&self, part_index: usize, range: crate::InclusiveRange) {}
+    fn part_started(&self, part_index: usize, range: crate::InclusiveRange) {
+        self.0.part_started(part_index, range);
+        self.1.part_started(part_index, range);
+    }
 
     fn part_completed(
         &self,
@@ -224,5 +245,7 @@ impl<RA: Reporter, RB: Reporter> Reporter for CompositeReporter<RA, RB> {
         n_bytes: usize,
         time: std::time::Duration,
     ) {
+        self.0.part_completed(part_index, n_chunks, n_bytes, time);
+        self.1.part_completed(part_index, n_chunks, n_bytes, time);
     }
 }
