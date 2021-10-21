638488b884e44a89bd4c88366a99a78e890d772c
Christian Douven <chridou@users.noreply.github.com>
2021-10-21 14:31:10 +0200

    #19 test and fix potential premature eof (#20)
    * stub for fs testing
    
    * reproduce bug
    
    * fixed buffer bug
    
    Co-authored-by: Christian Douven <christian.douven@users.noreply.github.com>

--- condow_fs/CHANGELOG.md
+++ condow_fs/CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.11.2] - 2021/10/21
+
+### FIX
+
+* Prefill of byte buffer
+
 ## [0.11.1] - 2021/10/21
 
 ### CHANGED
---
+++ condow_fs/tests/test.rs
@@ -0,0 +1,88 @@
+use condow_fs::{Condow, FsClient};
+
+fn create_condow_condow() -> Condow<FsClient> {
+    FsClient::condow(Default::default()).unwrap()
+}
+
+fn get_test_file_path() -> String {
+    // format!(
+    //     "{}/tests/test_data",
+    //     std::env::current_dir().unwrap().display()
+    // )
+    "/home/cdouven/git/condow/condow_fs/tests/test_data".to_string()
+}
+
+#[tokio::test]
+async fn download_full() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), ..)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"abcdefghijklmnopqrstuvwxyz");
+}
+
+#[tokio::test]
+async fn download_to() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), ..5)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"abcde");
+}
+
+#[tokio::test]
+async fn download_to_end() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), ..=26)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"abcdefghijklmnopqrstuvwxyz");
+}
+
+#[tokio::test]
+async fn download_from() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), 10..)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"klmnopqrstuvwxyz");
+}
+
+#[tokio::test]
+async fn download_from_to() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), 1..11)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"bcdefghijk");
+}
