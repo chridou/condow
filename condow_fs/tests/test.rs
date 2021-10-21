7c4c0306863541ac0091e9462e9300cd00a1db71
Christian Douven <christian.douven@users.noreply.github.com>
2021-10-21 14:19:11 +0200

    reproduce bug

--- condow_fs/tests/test.rs
+++ condow_fs/tests/test.rs
@@ -1,7 +1,27 @@
-use condow_fs::FsClient;
+use condow_fs::{Condow, FsClient};
 
+fn create_condow_condow() -> Condow<FsClient> {
+    FsClient::condow(Default::default()).unwrap()
+}
 
-#[test]
-fn test_x() {
-    panic!("blah");
+fn get_test_file_path() -> String {
+    format!(
+        "{}/tests/test_data",
+        std::env::current_dir().unwrap().display()
+    )
+}
+
+#[tokio::test]
+async fn download_full() {
+    let condow = create_condow_condow();
+
+    let data = condow
+        .download(get_test_file_path(), ..)
+        .await
+        .unwrap()
+        .into_vec()
+        .await
+        .unwrap();
+
+    assert_eq!(&data[..], b"abcdefghijklmnopqrstuvwxyz");
 }
